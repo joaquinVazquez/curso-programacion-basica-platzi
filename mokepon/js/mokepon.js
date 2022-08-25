d350257f3f695fe0b3ba8c664471f9b386686da6
joaquinvazquez <jromanvz@gmail.com>
2022-08-24 20:56:32 -0500

    Clase addEventListener, seleccionando mascota a traves de condicionales, dependiendo el input seleccionado, guardando en una variable para evaluarlo

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -3,7 +3,18 @@ function iniciarJuego(){
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 }
 function seleccionarMascotaJugador(){
-    alert("Tu jugador es: ");
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
+    if (inputHipodoge.checked){
+        alert("Seleccionaste a hipodoge")
+    } else if (inputCapipepo.checked){
+        alert("Seleccionaste a capipepo")
+    } else if (inputRatigueya.checked){
+        alert("Seleccionaste a ratigueya")
+    } else{
+        alert("Selecciona una mascota")
+    }
 }
 
 window.addEventListener('load', iniciarJuego);
