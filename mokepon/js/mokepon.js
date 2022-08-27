856b02d5205b83012a334abe861581aec22f3cf7
joaquinvazquez <jromanvz@gmail.com>
2022-08-26 19:25:22 -0500

    Clase manipulación del DOM - Utilizamos innerHTML para cambiar el html y sea interactivo

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -6,12 +6,14 @@ function seleccionarMascotaJugador(){
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
+
     if (inputHipodoge.checked){
-        alert("Seleccionaste a hipodoge")
+        spanMascotaJugador.innerHTML = "Hipodoge"
     } else if (inputCapipepo.checked){
-        alert("Seleccionaste a capipepo")
+        spanMascotaJugador.innerHTML = "Capipepo"
     } else if (inputRatigueya.checked){
-        alert("Seleccionaste a ratigueya")
+        spanMascotaJugador.innerHTML = "Ratigueya"
     } else{
         alert("Selecciona una mascota")
     }
