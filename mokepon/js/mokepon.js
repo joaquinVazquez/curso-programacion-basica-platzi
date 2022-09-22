10b56eeeac6ea855b3d719c6229ac596fae61e17
joaquinvazquez <jromanvz@gmail.com>
2022-09-22 08:07:18 -0500

    Clase objetos vs arrays

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -32,6 +32,7 @@ class Mokepon{
         this.nombre = nombre
         this.foto = foto
         this.vida = vida
+        this.ataques = []
     }
 }
 //Objetos
@@ -39,8 +40,29 @@ let hipodoge = new Mokepon('Hipodoge', './assets/mokepons_mokepon_hipodoge_attac
 let capipepo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.webp', 5)
 let ratigueya = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.webp', 5)
 
-mokepones.push(hipodoge, capipepo, ratigueya)
-console.log(mokepones)
+hipodoge.ataques.push(
+    {nombre:'💧', id:'boton-agua'},
+    {nombre:'💧', id:'boton-agua'},
+    {nombre:'💧', id:'boton-agua'},
+    {nombre:'🔥', id:'boton-fuego'},
+    {nombre:'🌱', id:'boton-tierra'}.
+)
+
+capipepo.ataques.push(
+    {nombre:'🌱', id:'boton-tierra'},
+    {nombre:'🌱', id:'boton-tierra'},
+    {nombre:'🌱', id:'boton-tierra'},
+    {nombre:'💧', id:'boton-agua'},
+    {nombre:'🔥', id:'boton-fuego'},
+)
+
+ratigueya.ataques.push(
+    {nombre:'🔥', id:'boton-fuego'},
+    {nombre:'🔥', id:'boton-fuego'},
+    {nombre:'🔥', id:'boton-fuego'},
+    {nombre:'💧', id:'boton-agua'},
+    {nombre:'🌱', id:'boton-tierra'},
+)
 
 function iniciarJuego(){
     sectionSeleccionarAtaque.style.display = 'none'
